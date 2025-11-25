4fdba911af2606e680ec964ad8eb34ceb177223e
Rajeev Singh <rajeevsingh19135@gmail.com>
2025-11-25 12:58:32 +0530

    Constructor

---
+++ Constructor.java
@@ -0,0 +1,18 @@
+package oops1;
+
+class Car{
+    String brand;
+    Car(String brand){
+        this.brand=brand;
+    } 
+    void show(){
+        System.out.println("brand="+brand);
+    }
+}
+
+public class Constructor {
+    public static void main(String[] args) {
+        Car c=new Car("Honda");
+        c.show();
+    }
+}

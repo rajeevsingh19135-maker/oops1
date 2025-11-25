5abf8b1f6a3ca9c5aca10ed0b653769e801fcaac
Rajeev Singh <rajeevsingh19135@gmail.com>
2025-11-25 16:40:14 +0530

    Encap1

---
+++ eccapsulation3.java
@@ -0,0 +1,19 @@
+package oops1;
+
+class Account{
+    private int balance;
+    public void setbalance(int b){
+        if(b>0)
+            balance=b;
+    }
+    public int getbalance(){
+        return balance;
+    }
+}
+public class eccapsulation3 {
+    public static void main(String[] args) {
+        Account a=new Account();
+        a.setbalance(-111);
+        System.out.println(a.getbalance());
+    }
+}
